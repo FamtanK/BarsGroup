b4894ffe92451b2a57e5ad6970fc919b0e079618
FamtanK <98282716+FamtanK@users.noreply.github.com>
2022-03-21 21:56:12 +0300

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Task1
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            char keyChar;
+            while ((keyChar = Console.ReadKey().KeyChar) != 'c')
+            {
+                OnKeyPressed?.Invoke(this, keyChar);
+            }
+        }
+    }
+}
