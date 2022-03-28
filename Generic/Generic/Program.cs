3c321cf24997a3c3377cb62f43c8c7b360363f61
FamtanK <98282716+FamtanK@users.noreply.github.com>
2022-03-28 21:46:08 +0300

    Add files via upload

---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Generic
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            LocalFileLogger<int> loger1 = new LocalFileLogger<int>("D:\\visual studio\\projects\\Generic\\File.txt");
+            loger1.LogInfo("message1");
+            loger1.LogWarning("message2");
+            loger1.LogError("message3", new Exception("error"));
+
+            LocalFileLogger<string> loger2 = new LocalFileLogger<string>("D:\\visual studio\\projects\\Generic\\File.txt");
+            loger2.LogInfo("message1");
+            loger2.LogWarning("message2");
+            loger2.LogError("message3", new Exception("error"));
+
+            LocalFileLogger<char> loger3 = new LocalFileLogger<char>("D:\\visual studio\\projects\\Generic\\File.txt");
+            loger3.LogInfo("message1");
+            loger3.LogWarning("message2");
+            loger3.LogError("message3", new Exception("error"));
+        }
+    }
+}
