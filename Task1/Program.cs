f49edcdcf8c5dc028278a873fa1bf80a2112402b
FamtanK <98282716+FamtanK@users.noreply.github.com>
2022-03-28 21:39:05 +0300

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1 task = new Task1();
+            task.OnKeyPressed += Print;
+            task.Run();
+        }
+
+        public static void Print(object sender, char keyChar)
+        {
+            Console.WriteLine(keyChar);
+        }
+    }
+}
